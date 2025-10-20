f4b086bdc27286915578d87f4862d00f6fad36e8
Deborah Emeni <deborahemeni@Deborahs-Laptop.local>
2025-10-20 11:22:13 +0100

    Use Northflank Postgres URI from environment variable

--- app/models/index.js
+++ app/models/index.js
@@ -1,16 +1,9 @@
-const dbConfig = require("../config/db.config.js");
+const { Sequelize } = require("sequelize");
 
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
+// Use the full database connection URL provided by Northflank
+const sequelize = new Sequelize(process.env.NF_NODE_CRUD_DEMO_DB_POSTGRES_URI, {
+  dialect: "postgres",
+  logging: false,
 });
 
 const db = {};
